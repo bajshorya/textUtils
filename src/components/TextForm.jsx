1922d0377806ed608361f5d221400c76ea9914d0
SHORYA BAJ <jain.shorya11@gmail.com>
2024-04-03 10:39:49 +0530

    first commit of textutils

---
+++ src/components/TextForm.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { useState } from "react";
+
+function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase", "success");
+  };
+  const handleLoClick = () => {
+    // console.log("uppercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase", "success");
+  };
+  const handleOnChange = (e) => {
+    // console.log("on change");
+    setText(e.target.value);
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          backgroundColor: props.mode === "dark" ? "#042743" : "white",
+          color: props.mode === "dark" ? "white" : "#042743", // Add this line
+        }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            placeholder="Enter Text here"
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+            style={{
+              backgroundColor: props.mode === "dark" ? "#13466e" : "white",
+              color: props.mode === "dark" ? "white" : "grey", // Add this line
+            }}
+          ></textarea>
+        </div>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleUpClick}
+        >
+          Convert To Uppercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleLoClick}
+        >
+          Convert To LowerCase
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{
+          backgroundColor: props.mode === "dark" ? "#042743" : "white",
+          color: props.mode === "dark" ? "white" : "#042743", // Add this line
+        }}
+      >
+        <h1>Your Text Summary</h1>
+        <p>
+          {
+            text.split(/\s+/).filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          Minutes reading time
+        </p>
+        <h3>Preview</h3>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter Something in the textbox above to preview it here"}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default TextForm;
